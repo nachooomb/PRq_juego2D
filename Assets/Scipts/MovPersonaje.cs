4db73263b973036fb21adf673352e59b6300937c
Nacho Medina Bernal <nachomedinabernal@gmail.com>
2024-03-15 14:12:25 +0100

    cinemachine, movement improve

--- Assets/Scipts/MovPersonaje.cs
+++ Assets/Scipts/MovPersonaje.cs
@@ -6,8 +6,9 @@ using UnityEngine;
 public class MovPersonaje : MonoBehaviour
 {
     public float speed = 1f;
-    public float JumpMultiplier = 2f;
+    public float JumpMultiplier = 4f;
 
+    bool AbleToJump = false;
 
     Rigidbody2D rb2D;
 
@@ -29,11 +30,11 @@ public class MovPersonaje : MonoBehaviour
         //transform.position = new Vector3 (transform.position.x + mov, transform.position.y, transform.position.z);
         transform.Translate(mov, 0, 0);
 
-        //SALTO   
-        if (Input.GetKeyDown(KeyCode.Space)){
-            rb2D.AddForce(new Vector2(0,JumpMultiplier), ForceMode2D.Impulse );
+        if (Input.GetKeyDown(KeyCode.LeftShift)){
+            speed = 3f;
+        } else if (Input.GetKeyUp(KeyCode.LeftShift)){
+            speed = 1f;
         }
-
 
         if (Input.GetKeyDown(KeyCode.A)){
             sp2D.flipX = true;
@@ -41,6 +42,33 @@ public class MovPersonaje : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.D)){
             sp2D.flipX = false;
         }
+
+
+        //SALTO   
+
+
+        //rayo para comprobar si el personaje toca el suelo 
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 0.5f);
+        
+        //Debug.DrawRay(transform.position, Vector2.down, Color.magenta);
+        // Debug.Log(hit.collider.name);
+
+        if (hit) {
+            if (hit.collider.name == "Grid"){
+                AbleToJump = true;
+            } else {
+                AbleToJump = false;
+            }
+        } else {
+            AbleToJump = false;
+        }
+
+        //salto si puedo saltar es true y pulso la tecla espacio
+        if (Input.GetKeyDown(KeyCode.Space) && AbleToJump==true){
+            rb2D.AddForce(new Vector2(0,JumpMultiplier), ForceMode2D.Impulse );
+        }
+
+
 
     }
 }
